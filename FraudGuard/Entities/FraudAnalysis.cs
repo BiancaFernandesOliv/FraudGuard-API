e82a68ddedd46dcc8ea1257fa8099f9a323353e9
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-06-20 18:28:37 -0300

    Criação teste

--- FraudGuard/Entities/FraudAnalysis.cs
+++ FraudGuard/Entities/FraudAnalysis.cs
@@ -4,11 +4,15 @@
         public bool IsSuspicious { get; private set; }
         public string RiskLevel { get; private set; }
         public string Reason { get; private set; }
+        public string Teste { get; private set; }
 
-        public FraudAnalysis(bool isSuspicious, string riskLevel, string reason) { 
+        public FraudAnalysis(bool isSuspicious, string riskLevel, string reason, string teste) { 
             IsSuspicious = isSuspicious;
             RiskLevel = riskLevel;
             Reason = reason;
+            Teste = teste;
         }
+
+      
     }
 }
