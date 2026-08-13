77e2bcc92548e26aedc5462a57bff7e5f9b50887
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-13 14:24:15 -0300

    refactor: ajusta responsabilidades do FraudAnalysisService

--- FraudGuard/Services/FraudAnalysisService.cs
+++ FraudGuard/Services/FraudAnalysisService.cs
@@ -1,12 +1,13 @@
 ﻿using FraudGuard.Entities;
 using FraudGuard.Enums;
+using FraudGuard.Interfaces;
 
 namespace FraudGuard.Services {
     public class FraudAnalysisService {
 
-        private readonly RiskAnalysisService _riskAnalysisService;
+        private readonly IRiskAnalysisService _riskAnalysisService;
 
-        public FraudAnalysisService(RiskAnalysisService riskAnalysisService) {
+        public FraudAnalysisService(IRiskAnalysisService riskAnalysisService) {
             _riskAnalysisService = riskAnalysisService;
         }
 
@@ -16,38 +17,44 @@ namespace FraudGuard.Services {
 
             bool isSuspicious = riskLevel != FraudRiskLevel.Low;
 
-            string reason;
+            string reason = DetermineReason(user, transaction, riskLevel);
 
-            if (isSuspicious) {
+            return new FraudAnalysis(isSuspicious, riskLevel, reason);
+        }
 
-                if (riskLevel == FraudRiskLevel.High) {
-                    reason = "High value international transaction at suspicious time.";
+        private string DetermineReason(User user, Transaction transaction, FraudRiskLevel riskLevel) {
+
+            if (riskLevel == FraudRiskLevel.Low) {
+                return "No suspicious behavior detected.";
+            }
+
+            if (riskLevel == FraudRiskLevel.Medium) {
+
+                if (transaction.IsInternational(user) && transaction.IsHighValue()) {
+                    return "High value international transaction.";
+                }
+                else if (transaction.IsInternational(user) && transaction.OccurredAtSuspiciousTime()) {
+                    return "International transaction at suspicious time.";
+                }
+                else if (transaction.IsInternational(user)) {
+                    return "International transaction.";
+                }
+                else if (transaction.IsHighValue() && transaction.OccurredAtSuspiciousTime()) {
+                    return "High value transaction at suspicious time.";
+                }
+                else if (transaction.IsHighValue()) {
+                    return "High value transaction.";
                 }
                 else {
-                    if(transaction.IsInternational(user) && transaction.IsHighValue()) {
-                        reason = "High value international transaction.";
-                    } 
-                    else if(transaction.IsInternational(user) && transaction.OccurredAtSuspiciousTime()) {
-                        reason = "International transaction at suspicious time.";
-                    }
-                    else if(transaction.IsInternational(user)) {
-                        reason = "International transaction.";
-                    }
-                    else if(transaction.IsHighValue() && transaction.OccurredAtSuspiciousTime()) {
-                        reason = "High value transaction at suspicious time.";
-                    } 
-                    else if (transaction.IsHighValue()) {
-                        reason = "High value transaction.";
-                    } 
-                    else {
-                        reason = "Transaction at suspicious time.";
-                    }
+                    return "Transaction at suspicious time.";
                 }
-            } 
-            else {
-                reason = "No suspicious behavior detected.";
             }
-            return new FraudAnalysis(isSuspicious, riskLevel, reason);
+
+            if (riskLevel == FraudRiskLevel.High) {
+                return "High value international transaction at suspicious time.";
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(riskLevel), riskLevel, "Invalid risk level.");
         }
 
     }
